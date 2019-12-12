fce7d37d88f7247bd11c9513fd5c084484b6fe55
honorzor <ioanntrapeznikov@gmail.com>
2019-12-12 23:47:25 +0300

    Added method for exit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,43 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
         primaryStage.setTitle("Tic-Noil");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
+        exitListener(primaryStage);
+    }
+
+    private void exitListener(Stage primaryStage) {
+        primaryStage.setOnCloseRequest(event -> {
+            if (!event.isConsumed()) {
+                final Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                final Optional<ButtonType> optionalButtonType = alert.showAndWait();
+                if (optionalButtonType.isPresent()) {
+                    final ButtonType button = optionalButtonType.get();
+                    if (button == ButtonType.CANCEL) {
+                        event.consume();
+                    }
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
