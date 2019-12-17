bdabb8a782e23372fa77b1f5ce3d2bc7d9a83865
saymynamej <h2so478521>
2019-12-17 22:44:47 +0300

    added db connection

---
+++ src/main/java/com/tic/noli/game/config/DBConnection.java
@@ -0,0 +1,18 @@
+package com.tic.noli.game.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private final static String url = "jdbc:sqlite:main.db";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+}
