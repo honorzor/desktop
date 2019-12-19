7ca0a28b05f36a7c4e924ddeb36b0d248f932a2c
honorzor <ioanntrapeznikov@gmail.com>
2019-12-20 00:16:46 +0300

    FIX

--- src/main/resources/query/CREATE_TABLE_USERS_QUERY.sql
+++ src/main/resources/query/CREATE_TABLE_USERS_QUERY.sql
@@ -2,7 +2,7 @@ create table if not exists users
 (
     id       INTEGER primary key autoincrement         NOT NULL,
     name     varchar(200) unique                       NOT NULL,
-    password varchar(200) check (length(password > 8)) NOT NULL,
+    password varchar(200) check (length(password) > 8) NOT NULL,
     email    varchar(200) unique                       NOT NULL
 );
 
